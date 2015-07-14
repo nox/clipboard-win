03dd0e8ffce94624e1938ee30f784d80a0718216
DoumanAsh <custparasite@gmx.se>
2015-07-14 23:51:19 +0300

    0.8.0: Follow-up

---
+++ doc/implementors/core/fmt/trait.Debug.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['clipboard_win'] = ["impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='struct' href='clipboard_win/struct.WinResult.html' title='clipboard_win::WinResult'>WinResult</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
