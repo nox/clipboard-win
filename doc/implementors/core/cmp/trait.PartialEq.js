4bce701f6ef13083ea12d07aad8ed7c022432a07
DoumanAsh <custparasite@gmx.se>
2015-07-30 20:58:32 +0300

    1.1.0: Follow-up

--- doc/implementors/core/cmp/trait.PartialEq.js
+++ doc/implementors/core/cmp/trait.PartialEq.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['clipboard_win'] = ["impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/cmp/trait.PartialEq.html' title='core::cmp::PartialEq'>PartialEq</a> for <a class='struct' href='clipboard_win/struct.WinResult.html' title='clipboard_win::WinResult'>WinResult</a>",];
+implementors['clipboard_win'] = ["impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/cmp/trait.PartialEq.html' title='core::cmp::PartialEq'>PartialEq</a> for <a class='struct' href='clipboard_win/struct.WindowsError.html' title='clipboard_win::WindowsError'>WindowsError</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
---
+++ doc/implementors/std/error/trait.Error.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['clipboard_win'] = ["impl <a class='trait' href='http://doc.rust-lang.org/nightly/std/error/trait.Error.html' title='std::error::Error'>Error</a> for <a class='struct' href='clipboard_win/struct.WindowsError.html' title='clipboard_win::WindowsError'>WindowsError</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
