1acf8c964b817d69a5c72e8b6dda7e5be6663f34
Douman <douman@gmx.se>
2015-11-21 20:51:18 +0300

    1.6.2: Doc follow-up

--- doc/implementors/core/clone/trait.Clone.js
+++ doc/implementors/core/clone/trait.Clone.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['clipboard_win'] = ["impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/clone/trait.Clone.html' title='core::clone::Clone'>Clone</a> for <a class='struct' href='clipboard_win/struct.WindowsError.html' title='clipboard_win::WindowsError'>WindowsError</a>",];
+implementors['clipboard_win'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/clone/trait.Clone.html' title='core::clone::Clone'>Clone</a> for <a class='struct' href='clipboard_win/struct.WindowsError.html' title='clipboard_win::WindowsError'>WindowsError</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
--- doc/implementors/core/fmt/trait.Debug.js
+++ doc/implementors/core/fmt/trait.Debug.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['clipboard_win'] = ["impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='struct' href='clipboard_win/struct.WindowsError.html' title='clipboard_win::WindowsError'>WindowsError</a>",];
+implementors['clipboard_win'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='struct' href='clipboard_win/struct.WindowsError.html' title='clipboard_win::WindowsError'>WindowsError</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
